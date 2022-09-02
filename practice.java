bb574afeded96ba47ea4b47b70ad95f033b5fe73
Shashank Sangale <sangaleshashank2001@gmail.com>
2022-09-02 14:09:45 +0530

    initial commit

---
+++ practice.java
@@ -0,0 +1,9 @@
+class practice{
+
+    public static void main(String[] args){
+        int a =10 ,b=20;
+        System.out.println("This is the result after adding two nos"+ (a+b));
+        System.out.println(b/a);
+
+    }
+}
---
+++ practice2.java
@@ -0,0 +1,9 @@
+public class practice2 {
+
+    public static void main(String[] args) {
+        int a=20,b=10;
+        System.out.println(a-b);
+        System.out.println(a*b);
+    }
+    
+}
