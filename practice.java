4708cd25e660d8c95988d86307477a30c7401b6d
Shashank Sangale <sangaleshashank2001@gmail.com>
2022-09-02 14:33:23 +0530

    3rd commit

--- practice.java
+++ practice.java
@@ -5,5 +5,6 @@ class practice{
         System.out.println("This is the result after adding two nos"+ (a+b));
         System.out.println("This is the result after dividing two nos",(b/a));
 
+        System.out.println("Hii this is shashanks new prrogram");
     }
 }
