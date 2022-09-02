4541db848d57af08713de2f861f1cf814dbb2650
Shashank Sangale <sangaleshashank2001@gmail.com>
2022-09-02 14:14:00 +0530

    2nd commit

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ class practice{
     public static void main(String[] args){
         int a =10 ,b=20;
         System.out.println("This is the result after adding two nos"+ (a+b));
-        System.out.println(b/a);
+        System.out.println("This is the result after dividing two nos",(b/a));
 
     }
 }
--- practice2.java
+++ practice2.java
@@ -2,8 +2,8 @@ public class practice2 {
 
     public static void main(String[] args) {
         int a=20,b=10;
-        System.out.println(a-b);
-        System.out.println(a*b);
+        System.out.println("this is the result after subctration of two nos",(a-b));
+        System.out.println("This is the result after multiplication of two nos",(a*b));
     }
     
 }
