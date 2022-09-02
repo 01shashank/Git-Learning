1a082a6055de9b0561cf16a45b79841f96644331
Shashank Sangale <sangaleshashank2001@gmail.com>
2022-09-02 14:34:24 +0530

    4th commit

--- practice2.java
+++ practice2.java
@@ -4,6 +4,8 @@ public class practice2 {
         int a=20,b=10;
         System.out.println("this is the result after subctration of two nos",(a-b));
         System.out.println("This is the result after multiplication of two nos",(a*b));
+
+        System.out.println("Hii this is shashanks 4th commit");
     }
     
 }
