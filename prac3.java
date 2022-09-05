e68649bd518942ef0852aeeab9af0b165f98456e
Shashank Sangale <sangaleshashank2001@gmail.com>
2022-09-05 14:35:45 +0530

    Fifth commit

--- prac3.java
+++ prac3.java
@@ -1,5 +1,6 @@
 public class prac3 {
     public static void main(String[] args) {
+        System.out.println("This is tenth commit");
         System.out.println("The third file and new commit to github");
     }
 }
